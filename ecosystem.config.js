9e6bdad080ad2b1da1c1d7910228f4f034ccdca8
RyanGaplek <92612190+RyanGaplek@users.noreply.github.com>
2026-03-15 17:54:17 +0700

    Add files via upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,44 @@
 module.exports = {
   apps: [
-    // 1. Ini adalah "Pancingan" agar Render tidak mematikan server
     { 
-      name: "Dummy-Web", 
-      script: "python3", 
-      args: "-m http.server 10000" 
-    },
-    // 2. Pasukan bot utama kamu
-    { 
-      name: "Tuyul-1", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel25", API_KEY: "mr_live_G-cKobZd69qx_QCW_hQQa1fF1WQAyYnK" }
+      name: "VIP-Max", script: "botpremium.py", interpreter: "python3",
+      env: { 
+        BOT_NAME: "Peaxel20", 
+        API_KEY: process.env.API_20, 
+        PRIVATE_KEY: process.env.PRIV_20 
+      }
     },
     { 
-      name: "Tuyul-2", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel26", API_KEY: "mr_live_m_vSW6HrB9Iv1uCBucsJwZTLuDPE-mq3" }
+      name: "VIP-Lite", script: "botpremium.py", interpreter: "python3",
+      env: { 
+        BOT_NAME: "Peaxel21", 
+        API_KEY: process.env.API_21, 
+        PRIVATE_KEY: process.env.PRIV_21 
+      }
     },
     { 
-      name: "Tuyul-3", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel27", API_KEY: "mr_live_FtfNNIVJMI1uZ14qdsqPhwxTIWUwqjVE" }
+      name: "VIP-Turbo", script: "botpremium.py", interpreter: "python3",
+      env: { 
+        BOT_NAME: "Peaxel22", 
+        API_KEY: process.env.API_22, 
+        PRIVATE_KEY: process.env.PRIV_22 
+      }
     },
     { 
-      name: "Tuyul-4", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel28", API_KEY: "mr_live_nL4-puQZ_VvbFC4Nkxjfz8bJrycoYxzo" }
+      name: "VIP-Dex", script: "botpremium.py", interpreter: "python3",
+      env: { 
+        BOT_NAME: "Peaxel23", 
+        API_KEY: process.env.API_23, 
+        PRIVATE_KEY: process.env.PRIV_23 
+      }
     },
     { 
-      name: "Tuyul-5", script: "bot.py", interpreter: "python3",
-      env: { BOT_NAME: "peaxel29", API_KEY: "mr_live_OXq1Z4oY7xoWlzppv9vcfzsoUuicE-cl" }
+      name: "VIP-Solar", script: "botpremium.py", interpreter: "python3",
+      env: { 
+        BOT_NAME: "Peaxel24", 
+        API_KEY: process.env.API_24, 
+        PRIVATE_KEY: process.env.PRIV_24 
+      }
     }
   ]
 };
