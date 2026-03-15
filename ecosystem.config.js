6789401c9a6d0517975cfa9e29f13c51d318ed7b
RyanGaplek <92612190+RyanGaplek@users.noreply.github.com>
2026-03-15 17:57:43 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,35 +1,35 @@
 module.exports = {
   apps: [
     { 
-      name: "peaxel25", script: "botpremium.py", interpreter: "python3",
+      name: "peaxel25", script: "bot.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel25", 
         API_KEY: process.env.API_25
       }
     },
     { 
-      name: "peaxel26", script: "botpremium.py", interpreter: "python3",
+      name: "peaxel26", script: "bot.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel26", 
         API_KEY: process.env.API_26
       }
     },
     { 
-      name: "peaxel27", script: "botpremium.py", interpreter: "python3",
+      name: "peaxel27", script: "bot.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel27", 
         API_KEY: process.env.API_27
       }
     },
     { 
-      name: "peaxel28", script: "botpremium.py", interpreter: "python3",
+      name: "peaxel28", script: "bot.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel28", 
         API_KEY: process.env.API_28
       }
     },
     { 
-      name: "peaxel29", script: "botpremium.py", interpreter: "python3",
+      name: "peaxel29", script: "bot.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel29", 
         API_KEY: process.env.API_29
