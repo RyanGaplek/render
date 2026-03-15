cda90db596e4d305a4d48c5280bd911ba13fe1f0
RyanGaplek <92612190+RyanGaplek@users.noreply.github.com>
2026-03-15 17:57:09 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,40 +4,35 @@ module.exports = {
       name: "peaxel25", script: "botpremium.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel25", 
-        API_KEY: process.env.API_25, 
-        PRIVATE_KEY: process.env.PRIV_25
+        API_KEY: process.env.API_25
       }
     },
     { 
       name: "peaxel26", script: "botpremium.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel26", 
-        API_KEY: process.env.API_26, 
-        PRIVATE_KEY: process.env.PRIV_26 
+        API_KEY: process.env.API_26
       }
     },
     { 
       name: "peaxel27", script: "botpremium.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel27", 
-        API_KEY: process.env.API_27, 
-        PRIVATE_KEY: process.env.PRIV_27 
+        API_KEY: process.env.API_27
       }
     },
     { 
       name: "peaxel28", script: "botpremium.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel28", 
-        API_KEY: process.env.API_28, 
-        PRIVATE_KEY: process.env.PRIV_28 
+        API_KEY: process.env.API_28
       }
     },
     { 
       name: "peaxel29", script: "botpremium.py", interpreter: "python3",
       env: { 
         BOT_NAME: "Peaxel29", 
-        API_KEY: process.env.API_29, 
-        PRIVATE_KEY: process.env.PRIV_29 
+        API_KEY: process.env.API_29
       }
     }
   ]
